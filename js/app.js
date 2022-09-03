d1e156bdb669c9c1001e64a8f2925d23716c063b
mamun102076 <mamun102076@gmail.com>
2022-09-03 17:35:27 +0600

    added modal

--- js/app.js
+++ js/app.js
@@ -25,40 +25,59 @@ const singleNewsItem = async (category_id) => {
     categoryShow(json.data)
 }
 const categoryShow = (categoryItem) => {
-    console.log(categoryItem)
-
     noData = document.getElementById('no-data')
     if (categoryItem.length === 0) {
         noData.classList.remove('d-none')
     }else{
         noData.classList.add('d-none')
     }
-    
     categoryItem.forEach((categoryItem) => {
         displayNewsItem = document.getElementById('display-news-item')
         cardDiv = document.createElement('div')
         cardDiv.classList.add('col')
         cardDiv.innerHTML = `
             <div class="card">
-                <img src="${categoryItem.image_url}" class="card-img-top" alt="...">
+                <img src="${categoryItem.image_url ? categoryItem.image_url : 'no image found'}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">'${categoryItem.title ? categoryItem.title : 'no title found'}'</h5>
-                    <p class="card-text">${categoryItem.details.slice(0,100)}</p>
+                    <p class="card-text">${categoryItem.details ? categoryItem.details.slice(0,100) : 'no details found'}</p>
                     <div class="d-flex justify-content-around align-items-center">
                         <div>
-                            <img src="${categoryItem.author.img}" style="height: 40px;width:40px;border-radius: 50px">
-                            <span>${categoryItem.author.name}<span>
+                            <img src="${categoryItem.author ? categoryItem.author.img : 'author image not found'}" style="height: 40px;width:40px;border-radius: 50px">
+                            <span>${categoryItem.author.name ? categoryItem.author.name : 'author name not found'}<span>
                         </div>
                         <div>
                             <span><i class="fa-solid fa-eye"></i></span>
-                            <span>${categoryItem.total_view}</span>
+                            <span>${categoryItem.total_view ? categoryItem.total_view : 'total view not found'}</span>
                         </div>
                     </div>
+                    <button onclick="showDetails('${categoryItem._id}')" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Show details</button>
                 </div>
             </div>
         `
         displayNewsItem.appendChild(cardDiv)
     })
+}
+
+
+
+const showDetails = async (news_id) => {
+    url = `https://openapi.programming-hero.com/api/news/${news_id}`
+    res = await fetch(url)
+    json = await res.json()
+    displayDetails(json.data[0])
+}
+const displayDetails = (modal) => {
+    console.log(modal)
+    newsModal = document.getElementById('newsModal')
+    newsModal.innerText = modal.title
+    modalBody = document.getElementById('modalBody')
+    modalBody.innerHTML = `
+        <p>${modal.details ? modal.details.slice(0,100) : 'no details found'}</p>
+        <p>Author name : ${modal.author.name ? modal.author.name : 'no name found'}</p>
+        <p>Pblished Date : ${modal.author.published_date ? modal.author.published_date : 'no date found'}</p>
+        <p>Total View : ${modal.total_view ? modal.total_view : 'no views found'}</p>
+    `
 }
 
 categoryLoad()
